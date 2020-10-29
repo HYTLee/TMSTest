d85674ca341691924d4f25c12271c0255c46f6bf
Yauheni Hramiashkevich <yauheni.hramiashkevich@apalon.com>
2020-10-29 20:05:12 +0300

    Initial commit

---
+++ TestAppForGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppForGit
+//
+//  Created by AP Yauheni Hramiashkevich on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
